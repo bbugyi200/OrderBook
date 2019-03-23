40ce577e87639a0136f3bbb82270891317039f62
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-23 15:09:41 -0400

    Update OrderBook

--- order_book.py
+++ order_book.py
@@ -4,26 +4,29 @@ class OrderBook:
         self.Asks = OrderNode()
 
     def bid(self, size, limit):
-        key = lambda x, y: x <= y
-        ask_size, ask_limit = self.search(self.Asks, limit, key=key)
-
-        if ask_size < 0:
-            self.add_node(self.Bids, size, limit)
-        elif ask_size < size:
-            self.bid(size - ask_size, limit)
-        elif size < ask_size:
-            self.add_node(self.Asks, ask_size - size, ask_limit)
+        self.order(N1=self.Bids,
+                   N2=self.Asks,
+                   key=lambda x, y: x <= y,
+                   size=size,
+                   limit=limit)
 
     def ask(self, size, limit):
-        key = lambda x, y: x >= y
-        bid_size, bid_limit = self.search(self.Bids, limit, key=key)
+        self.order(N1=self.Asks,
+                   N2=self.Bids,
+                   key=lambda x, y: x >= y,
+                   size=size,
+                   limit=limit)
 
-        if bid_size < 0:
-            self.add_node(self.Asks, size, limit)
-        elif bid_size < size:
-            self.bid(size - bid_size, limit)
-        elif size < bid_size:
-            self.add_node(self.Bids, bid_size - size, bid_limit)
+    def order(self, N1, N2, key, size, limit):
+        n2_size, n2_limit = self.search(N2, limit, key=key)
+
+        new_size = abs(size - n2_size)
+        if n2_size == 0:
+            self.add_node(N1, size, limit)
+        elif n2_size < size:
+            self.order(N1, N2, key, new_size, limit)
+        elif size < n2_size:
+            self.add_node(N2, new_size, n2_limit)
 
     def add_node(self, head, size, limit):
         if head.size is None and head.limit is None:
@@ -36,15 +39,17 @@ class OrderBook:
         curr = prev = head
         while curr is not None:
             prev = curr
-            if curr.limit >= limit:
+            if limit <= curr.limit:
                 curr = curr.left
             else:
                 curr = curr.right
 
-        if prev.limit >= limit:
+        if limit <= prev.limit:
             prev.left = new_node
         else:
             prev.right = new_node
+
+        new_node.parent = prev
 
     def search(self, head, target_limit, key):
         pass
