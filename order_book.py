932c7f78288b3d97bd8dc32b38538a28e896dbcd
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-23 15:15:34 -0400

    Update OrderBook

--- order_book.py
+++ order_book.py
@@ -18,41 +18,17 @@ class OrderBook:
                    limit=limit)
 
     def order(self, N1, N2, key, size, limit):
-        n2_size, n2_limit = self.search(N2, limit, key=key)
+        search_size, search_limit = search_tree(N2, limit, key=key)
 
-        new_size = abs(size - n2_size)
-        if n2_size == 0:
-            self.add_node(N1, size, limit)
-        elif n2_size < size:
-            self.order(N1, N2, key, new_size, limit)
-        elif size < n2_size:
-            self.add_node(N2, new_size, n2_limit)
-
-    def add_node(self, head, size, limit):
-        if head.size is None and head.limit is None:
-            head.size = size
-            head.limit = limit
+        if search_size == 0:
+            add_tree_node(N1, size, limit)
             return
 
-        new_node = OrderNode(size, limit)
-
-        curr = prev = head
-        while curr is not None:
-            prev = curr
-            if limit <= curr.limit:
-                curr = curr.left
-            else:
-                curr = curr.right
-
-        if limit <= prev.limit:
-            prev.left = new_node
-        else:
-            prev.right = new_node
-
-        new_node.parent = prev
-
-    def search(self, head, target_limit, key):
-        pass
+        new_size = abs(size - search_size)
+        if search_size < size:
+            self.order(N1, N2, key, new_size, limit)
+        elif size < search_size:
+            add_tree_node(N2, new_size, search_limit)
 
 
 class OrderNode:
@@ -60,3 +36,34 @@ class OrderNode:
         self.left = self.right = self.parent = None
         self.size = size
         self.limit = limit
+
+
+#####################################################################
+#  Tree Algorithms                                                  #
+#####################################################################
+def add_tree_node(head, size, limit):
+    if head.size is None and head.limit is None:
+        head.size = size
+        head.limit = limit
+        return
+
+    new_node = OrderNode(size, limit)
+
+    curr = prev = head
+    while curr is not None:
+        prev = curr
+        if limit <= curr.limit:
+            curr = curr.left
+        else:
+            curr = curr.right
+
+    if limit <= prev.limit:
+        prev.left = new_node
+    else:
+        prev.right = new_node
+
+    new_node.parent = prev
+
+
+def search_tree(head, target_limit, key):
+    pass
