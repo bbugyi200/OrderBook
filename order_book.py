8d483a54e500f8f3aa2f8d7d01059a5013a4e258
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-23 02:34:30 -0400

    Update OrderBook

--- order_book.py
+++ order_book.py
@@ -1,57 +1,57 @@
 class OrderBook:
     def __init__(self):
-        self.Bids = OrderTracker('Bids')
-        self.Asks = OrderTracker('Asks')
-
-    def __str__(self):
-        return '{}\n\n{}'.format(str(self.Bids), str(self.Asks))
-
-    def __repr__(self):
-        return '(Bids:{}, Asks:{})'.format(repr(self.Bids), repr(self.Asks))
+        self.Bids = OrderNode()
+        self.Asks = OrderNode()
 
     def bid(self, size, limit):
-        self.Bids.new(size, limit)
-        self.Bids -= self.Asks
+        key = lambda x, y: x <= y
+        ask_size, ask_limit = self.search(self.Asks, limit, key=key)
+
+        if ask_size < 0:
+            self.add_node(self.Bids, size, limit)
+        elif ask_size < size:
+            self.bid(size - ask_size, limit)
+        elif size < ask_size:
+            self.add_node(self.Asks, ask_size - size, ask_limit)
 
     def ask(self, size, limit):
-        self.Asks.new(size, limit)
-        self.Asks -= self.Bids
+        key = lambda x, y: x >= y
+        bid_size, bid_limit = self.search(self.Bids, limit, key=key)
 
+        if bid_size < 0:
+            self.add_node(self.Asks, size, limit)
+        elif bid_size < size:
+            self.bid(size - bid_size, limit)
+        elif size < bid_size:
+            self.add_node(self.Bids, bid_size - size, bid_limit)
 
-class OrderTracker:
-    def __init__(self, name):
-        self.name = name
-        self.sizes = []
-        self.limits = []
-        self.count = 0
+    def add_node(self, head, size, limit):
+        if head.size is None and head.limit is None:
+            head.size = size
+            head.limit = limit
+            return
 
-    def __sub__(self, rhs):
-        size, limit = self.sizes[-1], self.limits[-1]
+        new_node = OrderNode(size, limit)
 
-        while rhs.search(limit):
-            pass
+        curr = prev = head
+        while curr is not None:
+            prev = curr
+            if curr.limit >= limit:
+                curr = curr.left
+            else:
+                curr = curr.right
 
-        return self
+        if prev.limit >= limit:
+            prev.left = new_node
+        else:
+            prev.right = new_node
 
-    def __str__(self):
-        table = '\n'.join('{}\tat ${}'.format(size, limit)
-                          for size, limit in zip(self.sizes, self.limits))
-        return "========== {} ==========\n{}".format(self.name, table)
-
-    def __repr__(self):
-        return str(list(zip(self.sizes, self.limits)))
-
-    def new(self, size, limit):
-        self.sizes.append(size)
-        self.limits.append(limit)
-
-        self.count += 1
-
-    def pop(self):
-        self.sizes.pop()
-        self.limits.pop()
-
-        self.count -= 1
-
-    def search(self, limit):
+    def search(self, head, target_limit, key):
         pass
+
+
+class OrderNode:
+    def __init__(self, size=None, limit=None):
+        self.left = self.right = self.parent = None
+        self.size = size
+        self.limit = limit
