8b08a473119aaa5de5b1c302fa071e31f1bf45f1
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-24 16:43:14 -0400

    Update OrderBook

--- order_book.py
+++ order_book.py
@@ -4,33 +4,37 @@ class OrderBook:
         self.Asks = OrderNode()
 
     def bid(self, size, limit):
-        self.order(T1=self.Bids,
-                   T2=self.Asks,
-                   key=lambda x, y: x <= y,
-                   size=size,
-                   limit=limit)
+        self._order(T1=self.Bids,
+                    T2=self.Asks,
+                    key=lambda x, y: x <= y,
+                    size=size,
+                    limit=limit)
 
     def ask(self, size, limit):
-        self.order(T1=self.Asks,
-                   T2=self.Bids,
-                   key=lambda x, y: x >= y,
-                   size=size,
-                   limit=limit)
+        self._order(T1=self.Asks,
+                    T2=self.Bids,
+                    key=lambda x, y: x >= y,
+                    size=size,
+                    limit=limit)
 
-    def order(self, T1, T2, key, size, limit):
-        search_size, search_limit = search_tree(T2, limit, key=key)
-
-        if search_size == 0:
+    def _order(self, T1, T2, key, size, limit):
+        try:
+            node = search_tree(T2, limit, key=key)
+            search_size = node.size
+            search_limit = node.limit
+        except ValueError:
             add_tree_node(T1, size, limit)
-            return
+        else:
+            new_size = abs(size - search_size)
+            if size > search_size:
+                self._order(T1, T2, key, new_size, limit)
+            elif size < search_size:
+                add_tree_node(T2, new_size, search_limit)
 
-        new_size = abs(size - search_size)
-        if search_size < size:
-            self.order(T1, T2, key, new_size, limit)
-        elif size < search_size:
-            add_tree_node(T2, new_size, search_limit)
 
-
+#####################################################################
+#  Tree Algorithms                                                  #
+#####################################################################
 class OrderNode:
     def __init__(self, size=None, limit=None):
         self.left = self.right = self.parent = None
@@ -38,18 +42,15 @@ class OrderNode:
         self.limit = limit
 
 
-#####################################################################
-#  Tree Algorithms                                                  #
-#####################################################################
-def add_tree_node(head, size, limit):
-    if head.size is None and head.limit is None:
-        head.size = size
-        head.limit = limit
+def add_tree_node(T, size, limit):
+    if T.size is None and T.limit is None:
+        T.size = size
+        T.limit = limit
         return
 
     new_node = OrderNode(size, limit)
 
-    curr = prev = head
+    curr = prev = T
     while curr is not None:
         prev = curr
         if limit <= curr.limit:
@@ -57,13 +58,26 @@ def add_tree_node(head, size, limit):
         else:
             curr = curr.right
 
+    new_node.parent = prev
     if limit <= prev.limit:
         prev.left = new_node
     else:
         prev.right = new_node
 
-    new_node.parent = prev
 
-
-def search_tree(head, target_limit, key):
+def delete_tree_node(T, node):
     pass
+
+
+def search_tree(T, limit, key):
+    node = T
+    while node is not None:
+        if key(node.limit, limit):
+            delete_tree_node(T, node)
+            return node
+        elif limit <= node.limit:
+            node = node.left
+        else:
+            node = node.right
+
+    raise ValueError('Node not found.')
