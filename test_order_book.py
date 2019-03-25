74ea851ad9e0f3f616b5ed57e2f013c8c294bdaa
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-25 14:46:42 -0400

    Tests: added more assertions to test cases

--- test_order_book.py
+++ test_order_book.py
@@ -10,7 +10,9 @@ def test_order_book1(ob):
     ob.ask(300, 4)
 
     assert ob.Bids.size == 700
+    assert ob.Bids.limit == 5
     assert ob.Asks.size is None
+    assert ob.Asks.limit is None
 
 
 def test_order_book2(ob):
@@ -19,6 +21,7 @@ def test_order_book2(ob):
     ob.bid(1000, 5)
 
     assert ob.Bids.size is None
+    assert ob.Bids.limit is None
     assert ob.Asks.limit == 5
     assert ob.Asks.size == 200
 
@@ -28,13 +31,16 @@ def test_order_book3(ob):
     ob.ask(700, 6)
 
     assert ob.Bids.size == 1000
+    assert ob.Bids.limit == 5
     assert ob.Asks.size == 700
+    assert ob.Asks.limit == 6
 
     ob.ask(1600, 4.5)
 
     assert ob.Bids.size is None
-    assert ob.Asks.size == 700
+    assert ob.Bids.limit is None
     assert ob.Asks.left.size == 600
+    assert ob.Asks.left.limit == 4.5
 
 
 def test_delete_tree_node(ob):
@@ -44,6 +50,7 @@ def test_delete_tree_node(ob):
 
     order_book.delete_tree_node(ob.Asks)
 
+    assert ob.Asks.size == 400
     assert ob.Asks.limit == 3
 
 
