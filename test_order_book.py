ce824c58c11f16982ac8f1e110110b368c6f77ab
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-25 12:34:50 -0400

    Update OrderBook

--- test_order_book.py
+++ test_order_book.py
@@ -1,41 +1,41 @@
 import pytest
 
-import order_book as ob
+import order_book
 
 
-def test_order_book1(obook):
-    obook.bid(1000, 5)
-    obook.ask(300, 4)
+def test_order_book1(ob):
+    ob.bid(1000, 5)
+    ob.ask(300, 4)
 
-    assert obook.Bids.size == 700
-    assert obook.Asks.size is None
-
-
-def test_order_book2(obook):
-    obook.ask(700, 4)
-    print('Asks: %s' % (obook.Asks,))
-    obook.ask(500, 5)
-    print('Asks: %s' % (obook.Asks,))
-    obook.bid(1000, 5)
-    print('Asks: %s' % (obook.Asks,))
-
-    print('Bids: %s' % (obook.Bids,))
-
-    assert obook.Bids.size is None
-    assert obook.Asks.limit == 5
-    assert obook.Asks.size == 200
+    assert ob.Bids.size == 700
+    assert ob.Asks.size is None
 
 
-def test_delete_tree_node(obook):
-    obook.ask(700, 4)
-    obook.ask(500, 5)
-    obook.ask(400, 3)
+def test_order_book2(ob):
+    ob.ask(700, 4)
+    print('Asks: %s' % (ob.Asks,))
+    ob.ask(500, 5)
+    print('Asks: %s' % (ob.Asks,))
+    ob.bid(1000, 5)
+    print('Asks: %s' % (ob.Asks,))
 
-    ob.delete_tree_node(obook.Asks)
+    print('Bids: %s' % (ob.Bids,))
 
-    assert obook.Asks.limit == 3
+    assert ob.Bids.size is None
+    assert ob.Asks.limit == 5
+    assert ob.Asks.size == 200
+
+
+def test_delete_tree_node(ob):
+    ob.ask(700, 4)
+    ob.ask(500, 5)
+    ob.ask(400, 3)
+
+    order_book.delete_tree_node(ob.Asks)
+
+    assert ob.Asks.limit == 3
 
 
 @pytest.fixture
-def obook():
-    return ob.OrderBook()
+def ob():
+    return order_book.OrderBook()
