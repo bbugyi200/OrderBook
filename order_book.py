6b271f8d2ab2286ac3ea0e10fb227b8e70ef4718
Bryan Bugyi <bryanbugyi34@gmail.com>
2019-03-25 12:01:43 -0400

    Update OrderBook

--- order_book.py
+++ order_book.py
@@ -7,6 +7,7 @@ class OrderBook:
         self._order(T1=self.Bids,
                     T2=self.Asks,
                     key=lambda x, y: x <= y,
+                    direction='left',
                     size=size,
                     limit=limit)
 
@@ -14,20 +15,22 @@ class OrderBook:
         self._order(T1=self.Asks,
                     T2=self.Bids,
                     key=lambda x, y: x >= y,
+                    direction='right',
                     size=size,
                     limit=limit)
 
-    def _order(self, T1, T2, key, size, limit):
+    def _order(self, T1, T2, key, direction, size, limit):
         try:
-            node = search_tree(T2, limit, key=key)
+            node = search_tree(T2, limit, key, direction)
             search_size = node.size
             search_limit = node.limit
+            delete_tree_node(node)
         except ValueError:
             add_tree_node(T1, size, limit)
         else:
             new_size = abs(size - search_size)
             if size > search_size:
-                self._order(T1, T2, key, new_size, limit)
+                self._order(T1, T2, key, direction, new_size, limit)
             elif size < search_size:
                 add_tree_node(T2, new_size, search_limit)
 
@@ -40,6 +43,22 @@ class OrderNode:
         self.left = self.right = self.parent = None
         self.size = size
         self.limit = limit
+
+    def __str__(self):
+        return repr(self)
+
+    def __repr__(self):
+        if self.size is None:
+            return "Empty Tree"
+
+        ret = "(%d,%d)" % (self.size, self.limit)
+        if self.left is not None:
+            ret = repr(self.left) + ' <-- ' + ret
+
+        if self.right is not None:
+            ret = ret + ' --> ' + repr(self.right)
+
+        return ret
 
 
 def add_tree_node(T, size, limit):
@@ -65,19 +84,69 @@ def add_tree_node(T, size, limit):
         prev.right = new_node
 
 
-def search_tree(T, limit, key):
+def search_tree(T, limit, key, direction):
     node = T
-    while node is not None:
+    ret = None
+    while node is not None and node.limit is not None:
         if key(node.limit, limit):
-            delete_tree_node(T, node)
-            return node
+            ret = node
+            if direction == 'left':
+                node = node.left
+            elif direction == 'right':
+                node = node.right
         elif limit <= node.limit:
             node = node.left
         else:
             node = node.right
 
-    raise ValueError('Node not found.')
+    if ret is not None:
+        return ret
+    else:
+        raise ValueError('Node not found.')
 
 
-def delete_tree_node(T, node):
-    pass
+def delete_tree_node(node):
+    if node.parent is None and node.left is None and node.right is None:
+        node.limit = None
+        node.size = None
+        return
+
+    if node.left is None and node.right is None:
+        new_node = None
+    elif node.left is None:
+        new_node = node.right
+    elif node.right is None:
+        new_node = node.left
+    else:
+        temp = node
+        while temp.left is not None:
+            temp = temp.left
+
+        node.limit = temp.limit
+        node.size = temp.size
+        delete_tree_node(temp)
+        return
+
+    if node.parent is not None:
+        if node.parent.left == node:
+            node.parent.left = new_node
+        elif node.parent.right == node:
+            node.parent.right = new_node
+    else:
+        node.size = new_node.size
+        node.limit = new_node.limit
+        node.left = new_node.left
+        node.right = new_node.right
+
+
+if __name__ == '__main__':
+    ob = OrderBook()
+    ob.ask(400, 4)
+    ob.ask(500, 5)
+    ob.ask(300, 3)
+
+    ob.bid(200, 5)
+    ob.bid(50, 3)
+    ob.ask(250, 3)
+    print(ob.Asks)
+    print(ob.Bids)
--- test_order_book.py
+++ test_order_book.py
@@ -3,13 +3,39 @@ import pytest
 import order_book as ob
 
 
-def test_order_book():
-    obook = ob.OrderBook()
-
+def test_order_book1(obook):
     obook.bid(1000, 5)
     obook.ask(300, 4)
 
-    assert obook.Bids.count == 1
-    assert obook.Asks.count == 0
+    assert obook.Bids.size == 700
+    assert obook.Asks.size is None
 
-    assert obook.Bids.sizes[-1] == 700
+
+def test_order_book2(obook):
+    obook.ask(700, 4)
+    print('Asks: %s' % (obook.Asks,))
+    obook.ask(500, 5)
+    print('Asks: %s' % (obook.Asks,))
+    obook.bid(1000, 5)
+    print('Asks: %s' % (obook.Asks,))
+
+    print('Bids: %s' % (obook.Bids,))
+
+    assert obook.Bids.size is None
+    assert obook.Asks.limit == 5
+    assert obook.Asks.size == 200
+
+
+def test_delete_tree_node(obook):
+    obook.ask(700, 4)
+    obook.ask(500, 5)
+    obook.ask(400, 3)
+
+    ob.delete_tree_node(obook.Asks)
+
+    assert obook.Asks.limit == 3
+
+
+@pytest.fixture
+def obook():
+    return ob.OrderBook()
